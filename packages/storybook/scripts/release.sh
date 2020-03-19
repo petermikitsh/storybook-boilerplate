8d6c38bc73fc6091f9801e9f211167bdd7407ee2
Peter Mikitsh <peter.mikitsh@gmail.com>
2020-03-18 20:39:43 -0700

    chore(release.sh): remove --no-push flags

--- packages/storybook/scripts/release.sh
+++ packages/storybook/scripts/release.sh
@@ -14,9 +14,9 @@ SUBPACKAGE_DIR="$(dirname $( cd "$BASE_DIR" ; pwd -P ))"
 # 2. 'latest' folder
 SRC_DIR=$SUBPACKAGE_DIR/dist
 CURR_VERSION=$(node -p "require('${SUBPACKAGE_DIR}/package.json').version")
-npx --no-install gh-pages --dist $SRC_DIR --dest ./$CURR_VERSION --no-push --add
-npx --no-install gh-pages --dist $SRC_DIR --dest ./latest --no-push
+npx --no-install gh-pages --dist $SRC_DIR --dest ./$CURR_VERSION --add
+npx --no-install gh-pages --dist $SRC_DIR --dest ./latest
 
 # Copy storybook config to gh-pages root
 STORYBOOK_DIR=$SUBPACKAGE_DIR/.storybook
-npx --no-install gh-pages --dist $STORYBOOK_DIR --src storybook-config.json --no-push --add
+npx --no-install gh-pages --dist $STORYBOOK_DIR --src storybook-config.json --add

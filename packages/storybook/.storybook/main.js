811f005fe1728768c408b29e6b1b7e6dad6803af
Peter Mikitsh <peter.mikitsh@gmail.com>
2020-03-18 20:38:36 -0700

    chore: update deployment process

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,25 +1,8 @@
-const CopyPlugin = require("copy-webpack-plugin");
-const path = require("path");
-
 module.exports = {
   stories: ["../stories/**/*.stories.js"],
   addons: [
     "@storybook/addon-actions",
     "@storybook/addon-links",
     "@panosvoudouris/addon-versions"
-  ],
-  webpackFinal: async (config, { configType }) => {
-    if (configType === "PRODUCTION") {
-      config.plugins.push(
-        new CopyPlugin([
-          {
-            from: path.resolve(__dirname, "./storybook-config.json"),
-            to: path.resolve(__dirname, "../dist/storybook-config.json")
-          }
-        ])
-      );
-    }
-    // Return the altered config
-    return config;
-  }
+  ]
 };
--- packages/storybook/scripts/release.sh
+++ packages/storybook/scripts/release.sh
@@ -9,7 +9,14 @@ set -e
 BASE_DIR=$(dirname $0)
 SUBPACKAGE_DIR="$(dirname $( cd "$BASE_DIR" ; pwd -P ))"
 
-# Deploy to github pages
+# Deploy to github pages twice:
+# 1. version folder (e.g, '0.0.1')
+# 2. 'latest' folder
 SRC_DIR=$SUBPACKAGE_DIR/dist
 CURR_VERSION=$(node -p "require('${SUBPACKAGE_DIR}/package.json').version")
-npx --no-install gh-pages --dist $SRC_DIR --dest ./$CURR_VERSION --no-push
+npx --no-install gh-pages --dist $SRC_DIR --dest ./$CURR_VERSION --no-push --add
+npx --no-install gh-pages --dist $SRC_DIR --dest ./latest --no-push
+
+# Copy storybook config to gh-pages root
+STORYBOOK_DIR=$SUBPACKAGE_DIR/.storybook
+npx --no-install gh-pages --dist $STORYBOOK_DIR --src storybook-config.json --no-push --add

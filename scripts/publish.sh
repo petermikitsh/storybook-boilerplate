7ce0ea09832a9d585f6d9a821c9c3445005db169
Peter Mikitsh <peter.mikitsh@gmail.com>
2020-03-18 21:29:34 -0700

    chore: rename version to publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,6 +7,6 @@ set -x
 set -e
 
 npm run clean
-npx --no-install lerna version --conventional-commits --yes
+npx --no-install lerna version --conventional-commits --yes --no-git-tag-version
 npm run build
 npm run release

bea0a4daf65832861e7b468f517b08d96c7d031f
Peter Mikitsh <peter.mikitsh@gmail.com>
2020-03-19 01:35:49 -0700

    chore: debug gh-pages commands

--- packages/storybook/scripts/release.sh
+++ packages/storybook/scripts/release.sh
@@ -14,9 +14,16 @@ SUBPACKAGE_DIR="$(dirname $( cd "$BASE_DIR" ; pwd -P ))"
 # 2. 'latest' folder
 SRC_DIR=$SUBPACKAGE_DIR/dist
 CURR_VERSION=$(node -p "require('${SUBPACKAGE_DIR}/package.json').version")
-npx --no-install gh-pages --dist $SRC_DIR --dest ./$CURR_VERSION --add
-npx --no-install gh-pages --dist $SRC_DIR --dest ./latest
+NODE_DEBUG=gh-pages npx --no-install gh-pages --add \
+  --dist $SRC_DIR \
+  --dest ./$CURR_VERSION
+
+NODE_DEBUG=gh-pages npx --no-install gh-pages \
+  --dist $SRC_DIR \
+  --dest ./latest
 
 # Copy storybook config to gh-pages root
 STORYBOOK_DIR=$SUBPACKAGE_DIR/.storybook
-npx --no-install gh-pages --dist $STORYBOOK_DIR --src storybook-config.json --add
+NODE_DEBUG=gh-pages npx --no-install gh-pages \
+   --dist $STORYBOOK_DIR \
+   --src storybook-config.json
